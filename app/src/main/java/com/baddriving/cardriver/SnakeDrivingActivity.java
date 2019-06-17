cf83526915797bd2f0270d8570cda393c5615136
Dana Voros <vorosdana@gmail.com>
2019-06-17 11:27:15 -0700

    Added snake driving activity, fixed reference to it in main activity

---
+++ app/src/main/java/com/baddriving/cardriver/SnakeDrivingActivity.java
@@ -0,0 +1,14 @@
+package com.baddriving.cardriver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SnakeDrivingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_snake_driving);
+    }
+}
