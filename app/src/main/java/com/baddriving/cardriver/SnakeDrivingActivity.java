e0580af7d004399c853a3149744bb0aac5bd7503
Dana Voros <vorosdana@gmail.com>
2019-06-17 12:05:51 -0700

    Stubbed out click handlers for dpad

--- app/src/main/java/com/baddriving/cardriver/SnakeDrivingActivity.java
+++ app/src/main/java/com/baddriving/cardriver/SnakeDrivingActivity.java
@@ -3,6 +3,7 @@ package com.baddriving.cardriver;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SnakeDrivingActivity extends AppCompatActivity {
 
@@ -10,5 +11,18 @@ public class SnakeDrivingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snake_driving);
+    }
+
+    public void onLeftClick(View view){
+
+    }
+    public void onRightClick(View view){
+
+    }
+    public void onUpClick(View view){
+
+    }
+    public void onDownClick(View view){
+
     }
 }
